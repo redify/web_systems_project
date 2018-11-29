4301ac1c818f0efefa810671d78c2788025c50e9
unknown <gsjohnson667@gmail.com>
2018-11-28 20:38:36 -0500

    update week 9

--- code/range_get_obs_data.php
+++ code/range_get_obs_data.php
@@ -12,22 +12,33 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT timestamp,temp,rh,pressure FROM base_obs ORDER BY timestamp desc limit 1";
+$sql = "SELECT timestamp,temp,rh,pressure FROM base_obs WHERE timestamp > DATE_SUB(now(), INTERVAL 1 DAY) ORDER BY timestamp desc";
 $result = $conn->query($sql);
 
 if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
 }
 
-$row = $result->fetch_assoc();
+$dataObjArray = array();
 
+while($row = $result->fetch_assoc())
+{
+  $dataObj = new stdClass();
+  $dataObj->temp = $row["temp"];
+  $dataObj->rh = $row["rh"];
+  $dataObj->pressure = $row["pressure"];
+  $dataObj->timestamp = $row["timestamp"];
+
+  array_push($dataObjArray, $dataObj);
+}
+/*
 $dataObj = new stdClass();
 $dataObj->temp = $row["temp"];
 $dataObj->rh = $row["rh"];
 $dataObj->pressure = $row["pressure"];
 $dataObj->timestamp = $row["timestamp"];
-
-echo json_encode($dataObj);
+*/
+echo json_encode($dataObjArray);
 
 $conn->close();
 ?>
